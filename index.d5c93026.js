31c0e1565ab9d3a370ef90de91a5fd282bd0976c
CatarinaLeon <90268687+CatarinaLeon@users.noreply.github.com>
2022-07-20 17:20:55 +0000

    Deploying to gh-pages from @ CatarinaLeon/project-BarberShop@98023ac2862a327a55b123a7b9962ac24f593574 🚀

---
+++ index.d5c93026.js
@@ -0,0 +1,2 @@
+const refs={openModalBtn:document.querySelector("[data-open-btn]"),closeModalBtn:document.querySelector("[data-close-btn]"),backdrop:document.querySelector("[data-backdrop]")};function toggleModal(){refs.backdrop.classList.toggle("is-hidden")}refs.openModalBtn.addEventListener("click",toggleModal),refs.closeModalBtn.addEventListener("click",toggleModal);
+//# sourceMappingURL=index.d5c93026.js.map
